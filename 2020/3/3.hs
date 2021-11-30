cce803418b1281e3175de59483f130be6bd5bb17
Niklas Hedström <niklas.hedstrom@telavox.com>
2021-11-30 14:40:26 +0100

    first commit

--- 2020/3/3.hs
+++
@@ -1,7 +0,0 @@
-main = do
-  contents <- getContents
-  putStr (colitions contents 1 3)
-
-colitions :: [String] -> Int -> Int -> [Bool]
-colitions [] _ _ = False
-colitions xs r d = [(head xs) !! , colitions (drop d xs) (r  d]
--- visualizer/download.js
+++ visualizer/download.js
@@ -3,7 +3,7 @@ var https = require('https');
 var fs = require('fs');
 var options = {
     host: 'adventofcode.com',
-    path: '/2020/leaderboard/private/view/380632.json',
+    path: '/2021/leaderboard/private/view/1549974.json',
     method: 'GET',
     headers: {
         "Cookie": "session=53616c7465645f5f0aa26143183875c778d4c25921bb3b3b74dcf80ccf0361558099be833f649fd2b145acab2e5961b9", // POINTER its unfortunately useless
